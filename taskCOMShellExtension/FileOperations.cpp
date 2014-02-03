36e8c1b56fd7f37dbed6f7c89a5c5babce5509eb
alexandear <redykrom@gmail.com>
2014-02-04 00:40:06 +0200

    Add calculate checksum method

--- taskCOMShellExtension/FileOperations.cpp
+++ taskCOMShellExtension/FileOperations.cpp
@@ -4,16 +4,13 @@
 #include <sys/stat.h>
 #include "FileOperations.h"
 
-FileOperations::FileOperations() {
+FileOperations::FileOperations() { }
 
-}
-
-FileOperations::~FileOperations() {
-}
+FileOperations::~FileOperations() { }
 
 void FileOperations::addFile(const std::wstring & filePath) {
     std::wstring fileName(PathFindFileName(filePath.c_str()));
-    m_fileMap.insert ( FileInfoPair(fileName, getInfo(fileName, m_fileInfo)) );
+    m_fileMap.insert ( FileInfoPair(fileName, getInfo(filePath, m_fileInfo)) );
 }
 
 HRESULT FileOperations::writeLogFile(const std::wstring & logFile) {
@@ -22,10 +19,11 @@ HRESULT FileOperations::writeLogFile(const std::wstring & logFile) {
     wfout.open(logFile, wofstream::app | wofstream::out);
     if (wfout.is_open()) {
         char timebuf[26];
-        for (auto file : m_fileMap)
-        {
+        for (auto file : m_fileMap) {
             ctime_s(timebuf, 26, &file.second.creationTime);
-            wfout << file.first << L"   " << file.second.size << L" bytes   " << (timebuf + 4);
+            timebuf[24] = '\0';
+            wfout << file.first << L"   " << file.second.size << L" bytes   " << (timebuf + 4)
+                  << L"   " << file.second.perByteSum << std::endl;
         }
         wfout.close();
         return S_OK;
@@ -35,11 +33,27 @@ HRESULT FileOperations::writeLogFile(const std::wstring & logFile) {
 }
 
 
-FileOperations::FileInfo & FileOperations::getInfo(const std::wstring & fileName, FileInfo & fileInfo) {
+FileOperations::FileInfo & FileOperations::getInfo(const std::wstring & filePath, FileInfo & fileInfo) {
     struct _stat st;
-    if (_wstat(fileName.c_str(), &st) == 0) {
+    if (_wstat(filePath.c_str(), &st) == 0) {
         fileInfo.size = st.st_size;
         fileInfo.creationTime = st.st_ctime;
+        fileInfo.perByteSum = calculatePerByteSum(filePath);
     }
     return fileInfo;
+}
+
+DWORD FileOperations::calculatePerByteSum(const std::wstring & filePath) {
+    DWORD checksum = 0;
+    std::ifstream file(filePath, std::ios::binary);
+    if (file.is_open()) {
+        char byte;
+        while (!file.eof()) {
+            file.read(&byte, sizeof(char));
+            checksum += byte;
+        }
+        file.close();
+    }
+
+    return checksum;
 }
